565fce76d1e0e8210414b6fc3535dbef05415035
Petit Wenceslas <wenceslas.petit@gmail.com>
2020-09-25 13:54:59 +0200

    maj

---
+++ src/test/java/at/catalysts/tools/reversedependencies/ReverseDependenciesExtractionTest.java
@@ -0,0 +1,45 @@
+package at.catalysts.tools.reversedependencies;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+import at.catalysts.tools.reversedependencies.data.DependencyTree;
+import at.catalysts.tools.reversedependencies.data.Setting;
+import at.catalysts.tools.reversedependencies.io.DependencyFileReader;
+import at.catalysts.tools.reversedependencies.test.IntegrationTest;
+
+public class ReverseDependenciesExtractionTest {
+
+    @Category(IntegrationTest.class)
+    @Test
+    public void testMainWithCache() {
+        ReverseDependencies.main(new String[] {"-q", "src/test/resources/query.txt", "-m", "src/test/resources/inuse.csv",
+                "-r", "http://repo.appli.dgfip/nexus/content/repositories/releases/", "-o", "src/test/resources/resultats_extraction.csv",
+                "-s", "fr/gouv/finances/tpi", "-c", "src/test/resources/cache.csv"});
+       
+        
+        /*
+        DependencyFileReader reader = new DependencyFileReader("src/test/resources/results.csv");
+        reader.setReversedWithUsedByDependencies(true);
+        List<DependencyTree> resultTrees = reader.readDependencyTreeFile();
+        assertNotNull(resultTrees);
+        assertEquals(2, resultTrees.size());
+        assertEquals("queryArtifactId", resultTrees.get(0).getDependency().getArtifactId());
+        assertEquals("junit", resultTrees.get(1).getDependency().getArtifactId());
+        assertEquals("4.12", resultTrees.get(1).getDependency().getVersion());
+        assertEquals(1, resultTrees.get(1).getUsedBy().size());
+        assertEquals("maven-compiler-plugin", resultTrees.get(1).getUsedBy().get(0).getDependency().getArtifactId());
+        assertEquals("3.6.0", resultTrees.get(1).getUsedBy().get(0).getDependency().getVersion());
+        */
+    }
+
+ 
+}
